a332fe79edc0c9ff8623d0242b55dd170fd7dac8
pooriaabdollahi <pooria.abdollahi13@gmail.com>
2024-04-07 18:36:52 +0330

    Add project files.

---
+++ Domain/Models/ProductModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class ProductModel
+    {
+        public string Name { get; set; }
+        public DateTime ProduceDate { get; set; }
+        public int ManufacturePhone { get; set; }
+        public string ManufactureEmail {  get; set; }
+        public Boolean IsAvailable { get; set; }
+    }
+}
