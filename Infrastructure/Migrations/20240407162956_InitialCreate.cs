f199ad2dfa77a36153e31425f6c2d2b1cd0d3be6
pooriaabdollahi <pooria.abdollahi13@gmail.com>
2024-04-08 12:52:25 +0330

    added db migrations

---
+++ Infrastructure/Migrations/20240407162956_InitialCreate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ProductModel",
+                columns: table => new
+                {
+                    ProduceDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ManufactureEmail = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ManufacturePhone = table.Column<int>(type: "int", nullable: false),
+                    IsAvailable = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductModel", x => new { x.ManufactureEmail, x.ProduceDate });
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProductModel");
+        }
+    }
+}
---
+++ Infrastructure/MyDBContext.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+namespace productCrud.Infrastructure;
+
+
+internal class MyContext : DbContext
+{
+    static string connectionString = "Server=localhost,1435;Database=YourDatabaseName;User Id=dbo;Password=Myp@ssword1;";
+    public MyContext() : base(GetOptions(connectionString))
+    { }
+    public DbSet<ProductModel> Products { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<ProductModel>()
+            .HasKey(b => new { b.ManufactureEmail, b.ProduceDate });
+        foreach (IMutableEntityType entityType in modelBuilder.Model.GetEntityTypes())
+        {
+            modelBuilder.Entity(entityType.Name)
+            .ToTable(entityType.ClrType.Name);
+        }
+
+    }
+    private static DbContextOptions GetOptions(string connectionString)
+    {
+        return SqlServerDbContextOptionsExtensions.UseSqlServer(new DbContextOptionsBuilder(), connectionString).Options;
+    }
+}
